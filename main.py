749eaf88ef29448f11720cca35c6f3a02eb58761
sungminoh <smoh2044@gmail.com>
2018-01-27 22:54:29 -0800

    modulize

---
+++ main.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import platform
+from time import sleep
+from snulife import Snulife
+
+
+GMAIL_ACCOUNT = '<account without @gmail.com>'
+GMAIL_PASSWORD = '<password>'
+SNULIFE_ACCOUNT = '<snulife account>'
+SNULIFE_PASSWORD = '<snulife password>'
+TITLE = u'스누라이프 알리미 입니다.'
+SNULIFE_CONSTANT = {
+    'post_class': 'hx',
+    'target': 'http://snulife.com/housing/'
+}
+
+
+def get_args():
+    description = 'snulife new post alarm'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--user_id', '-u',
+                        # required=True,
+                        type=str,
+                        help='snulife user id')
+    parser.add_argument('--password', '-p',
+                        # required=True,
+                        type=str,
+                        help='snulife password')
+    parser.add_argument('--sender', '-s',
+                        type=str,
+                        help='sender email address or gmail id')
+    parser.add_argument('--gmail_password', '-gp',
+                        type=str,
+                        help='gmail account password if sender is gmail account')
+    parser.add_argument('--receiver', '-r',
+                        type=str,
+                        help='receiver (ex. asd@asd.net,qwe@qwe.com)')
+    parser.add_argument('--title', '-t',
+                        type=str,
+                        help='email subject')
+    parser.add_argument('--url', '-l',
+                        type=str,
+                        help='target url')
+    parser.add_argument('--filter', '-f',
+                        type=str,
+                        help='regex filter')
+    parser.add_argument('--keep_running', '-k',
+                        type=str2bool,
+                        default=False,
+                        help='target class name')
+    parser.add_argument('--class_name', '-c',
+                        type=str,
+                        help='target class name')
+    return vars(parser.parse_args())
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def to_unicode(s):
+    if isinstance(s, str):
+        return s.decode('utf-8')
+    else:
+        return s
+
+
+def main():
+    args = get_args()
+    # arguments
+    user_id = args.get('user_id', SNULIFE_ACCOUNT)
+    password = args.get('password', SNULIFE_PASSWORD)
+    sender = args['sender'] or platform.node()
+    gmail_password = args['password'] or ''
+    receiver = [email.strip() for email in (args['receiver'] or '').split(',')]
+    title = to_unicode(args['title'] or TITLE)
+    url = args['url'] or SNULIFE_CONSTANT['target']
+    regex = to_unicode(args['filter'] or '')
+    keep_running = args['keep_running']
+    class_name = args['class_name'] or SNULIFE_CONSTANT['post_class']
+
+    snulife = Snulife(user_id, password)
+    if keep_running:
+        while(True):
+            try:
+                snulife.crawl(url=url, class_name=class_name, regex=regex)\
+                    .noti(title=title, sender=sender, receiver=receiver, password=gmail_password)
+                print 'sleep ...'
+                sleep(5)
+                print 'wake up !!!'
+            except Exception as e:
+                print 'fail'
+                print e
+                print 'sleep ...'
+                sleep(60)
+                print 'wake up !!!'
+    else:
+        snulife.crawl(url=url, class_name=class_name, regex=regex)\
+            .noti(title=title, sender=sender, receiver=receiver, password=gmail_password)
+
+
+if __name__ == '__main__':
+    main()
--- snulife.py
+++ snulife.py
@@ -1,53 +1,14 @@
 # -*- coding: utf-8 -*-
-
 import requests
-from time import sleep
+import grequests
 from lxml import html
 from itertools import chain
 import cPickle as pk
-import smtplib
-from email.mime.text import MIMEText
-import argparse
-import platform
+from datetime import datetime
 import re
 import os
 
-
-GMAIL_ACCOUNT = '<account without @gmail.com>'
-GMAIL_PASSWORD = '<password>'
-SNULIFE_ACCOUNT = '<snulife account>'
-SNULIFE_PASSWORD = '<snulife password>'
-TITLE = u'스누라이프 알리미 입니다.'
-
-
-SNULIFE_CONSTANT = {
-    'post_class': 'hx',
-    'target': 'http://snulife.com/housing/'
-}
-
-
-class EmailHandler(object):
-    @staticmethod
-    def send_email(subject, sender, receiver, content, password=''):
-        msg = MIMEText(content.encode('utf-8'), _charset='utf-8')
-        msg['Subject'] = subject
-        msg['From'] = sender
-        msg['To'] = ', '.join(receiver)
-        try:
-            server = smtplib.SMTP("smtp.gmail.com", 587)
-            server.ehlo()
-            server.starttls()
-            server.login(sender.split('@')[0], password)
-            server.sendmail(sender, receiver, msg.as_string())
-            server.close()
-            print 'successfully sent the mail to %s' % msg['To']
-        except Exception:
-            s = smtplib.SMTP('localhost')
-            s.sendmail(sender, receiver, msg.as_string())
-            s.quit()
-            print "send through localhost"
-            print 'successfully sent the mail to %s' % msg['To']
-
+from helpers import EmailHandler
 
 class Snulife(object):
     saved = './tmp.pkl'
@@ -81,145 +42,55 @@ class Snulife(object):
 
     def connect(self):
         self.s.post(self.login, self.data, headers=self.header)
+        print 'Session connected'
         return self
 
     def get(self, url):
-        text = self.s.get(url).text
-        self.tree = html.fromstring(text)
+        print 'Crawl from urls below.'
+        print url
+        urls = url.split('\n')
+        reqs = [grequests.get(i, session=self.s) for i in urls]
+        resps = grequests.map(reqs)
+        texts = [resp.text for resp in resps if resp.status_code == 200]
+        self.trees = [html.fromstring(text) for text in texts]
+        print 'Crawl finished'
         return self
 
-    def is_same(self):
+    def load_saved(self):
+        ret = []
         if os.path.isfile(Snulife.saved):
             with open(Snulife.saved) as f:
-                return pk.load(f) == self.titles
-        return False
-
-    def make_content(self):
-        return '\n'.join(chain(*zip(self.titles, self.links)))
+                ret = pk.load(f)
+        return ret
 
     def find_all(self, class_name, regex=''):
-        self.titles = []
-        self.links = []
-        for e in self.tree.find_class(class_name):
-            link = Snulife.base + e.values()[0]
-            title = e.text_content().strip()
-            if not re.search(regex, title):
-                continue
-            self.links.append(link)
-            self.titles.append(title)
+        print "Finding all classname:%s containing '%s' ..." % (class_name, regex),
+        self.posts = []
+        for tree in self.trees:
+            for e in tree.find_class(class_name):
+                link = e.values()[0]
+                title = e.text_content().strip()
+                if not re.search(regex, title):
+                    continue
+                self.posts.append((title, link))
+        print 'Done'
         return self
 
-    def send_email(self, title, sender, receiver, password=''):
-        content = self.make_content()
-        sender = sender if '@' in sender else '%s@gmail.com' % sender
-        EmailHandler.send_email(title, sender, receiver, content, password)
-
-    def noti(self, title, sender, receiver, password=''):
-        content = self.make_content()
-        print content
-        if self.is_same():
-            print 'no new post'
+    def noti(self, title, sender, receiver, content='', password=''):
+        print 'Crawled posts'
+        print '\n'.join(chain(*self.posts))
+        saved = self.load_saved()
+        if saved == self.posts:
+            print 'No new post'
         else:
-            print 'got new post'
+            print 'Got new post'
             with open(Snulife.saved, 'wb') as f:
-                pk.dump(self.titles, f)
-                self.send_email(title, sender, receiver, password)
+                pk.dump(self.posts, f)
+            new_posts = [p for p in self.posts if p not in saved]
+            print 'New posts'
+            print '\n'.join(chain(*new_posts))
+            EmailHandler.send_email(title, sender, receiver, content, password)
 
     def crawl(self, url='', class_name='', regex=''):
         self.connect().get(url).find_all(class_name, regex)
-        print self.make_content()
         return self
-
-
-def get_args():
-    description = 'snulife new post alarm'
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('--user_id', '-u',
-                        # required=True,
-                        type=str,
-                        help='snulife user id')
-    parser.add_argument('--password', '-p',
-                        # required=True,
-                        type=str,
-                        help='snulife password')
-    parser.add_argument('--sender', '-s',
-                        type=str,
-                        help='sender email address or gmail id')
-    parser.add_argument('--gmail_password', '-gp',
-                        type=str,
-                        help='gmail account password if sender is gmail account')
-    parser.add_argument('--receiver', '-r',
-                        type=str,
-                        help='receiver (ex. asd@asd.net,qwe@qwe.com)')
-    parser.add_argument('--title', '-t',
-                        type=str,
-                        help='email subject')
-    parser.add_argument('--url', '-l',
-                        type=str,
-                        help='target url')
-    parser.add_argument('--filter', '-f',
-                        type=str,
-                        help='regex filter')
-    parser.add_argument('--keep_running', '-k',
-                        type=str2bool,
-                        default=False,
-                        help='target class name')
-    parser.add_argument('--class_name', '-c',
-                        type=str,
-                        help='target class name')
-    return vars(parser.parse_args())
-
-
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-
-def to_unicode(s):
-    if isinstance(s, str):
-        return s.decode('utf-8')
-    else:
-        return s
-
-
-def main():
-    args = get_args()
-    # arguments
-    user_id = args.get('user_id', SNULIFE_ACCOUNT)
-    password = args.get('password', SNULIFE_PASSWORD)
-    sender = args['sender'] or platform.node()
-    gmail_password = args['password'] or ''
-    receiver = [email.strip() for email in (args['receiver'] or '').split(',')]
-    title = to_unicode(args['title'] or TITLE)
-    url = args['url'] or SNULIFE_CONSTANT['target']
-    regex = to_unicode(args['filter'] or '')
-    keep_running = args['keep_running']
-    class_name = args['class_name'] or SNULIFE_CONSTANT['post_class']
-
-    snulife = Snulife(user_id, password)
-    if keep_running:
-        while(True):
-            try:
-                snulife.crawl(url=url, class_name=class_name, regex=regex)\
-                    .noti(title=title, sender=sender, receiver=receiver, password=gmail_password)
-                print 'sleep ...'
-                sleep(5)
-                print 'wake up !!!'
-            except Exception as e:
-                print 'fail'
-                print e
-                print 'sleep ...'
-                sleep(60)
-                print 'wake up !!!'
-    else:
-        snulife.crawl(url=url, class_name=class_name, regex=regex)\
-            .send_email(title=title, sender=sender, receiver=receiver, password=gmail_password)
-
-
-if __name__ == '__main__':
-    main()
-
